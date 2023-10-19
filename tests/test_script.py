be7ede3bad2f55a2035e985ecfb326d5ca34e6ec
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2023-10-19 21:43:21 +1100

    refactor: better variable names

--- tests/test_script.py
+++ tests/test_script.py
@@ -8,35 +8,35 @@ class TestSecondsToTime:
     Unit tests for script.py
     """
 
-    assert_msg = "Expected {}, got {}"
+    expected_assert_msg = "Expected {}, got {}"
     
-    def test_seconds_to_time_1(self):
+    def test_output_1(self):
         test_input = 0
         expected = (0, 0, 0)
         actual = seconds_to_time(test_input)
-        assert expected == actual, self.assert_msg.format(expected, actual)
+        assert expected == actual, self.expected_assert_msg.format(expected, actual)
 
-    def test_seconds_to_time_2(self):
+    def test_output_2(self):
         test_input = 10
         expected = (0, 0, 10)
         actual = seconds_to_time(test_input)
-        assert expected == actual, self.assert_msg.format(expected, actual)
+        assert expected == actual, self.expected_assert_msg.format(expected, actual)
 
-    def test_seconds_to_time_3(self):
+    def test_output_3(self):
         test_input = 60
         expected = (0, 1, 0)
         actual = seconds_to_time(test_input)
-        assert expected == actual, self.assert_msg.format(expected, actual)
+        assert expected == actual, self.expected_assert_msg.format(expected, actual)
 
-    def test_seconds_to_time_4(self):
+    def test_output_4(self):
         test_input = 3600
         expected = (1, 0, 0)
         actual = seconds_to_time(test_input)
-        assert expected == actual, self.assert_msg.format(expected, actual)
+        assert expected == actual, self.expected_assert_msg.format(expected, actual)
 
-    def test_seconds_to_time_5(self):
+    def test_output_5(self):
         test_input = 3661
         expected = (1, 1, 1)
         actual = seconds_to_time(test_input)
-        assert expected == actual, self.assert_msg.format(expected, actual)
+        assert expected == actual, self.expected_assert_msg.format(expected, actual)
 
